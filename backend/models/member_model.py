37d816aadff851baf54a5e6cfc24b8b7fab035d0
JB55Matthews <blackhedley25@gmail.com>
2025-07-12 17:45:47 -0230

    small progress

--- backend/models/member_model.py
+++ backend/models/member_model.py
@@ -1,4 +1,5 @@
 from extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Member(db.Model):
     __tablename__ = "members"
@@ -10,3 +11,15 @@ class Member(db.Model):
     password = db.Column(db.Sring(200))
 
     famiy = db.relationship("Family", back_populates="members")
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        if not self.password_hash:
+            return False
+        return check_password_hash(self.password_hash, password)
+    
+    @property
+    def has_password(self):
+        return self.password_hash is not None
--- backend/routes/family_route.py
+++ backend/routes/family_route.py
@@ -1,12 +1,25 @@
-from flask_jwt_extended import jwt_required
+from flask import request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_smorest import Blueprint
-from flask.views import MethodView
 from models.family_model import Family
+from models.member_model import Member
 from extensions import db
 
 bp = Blueprint("familys", __name__)
 
 @bp.route("/familys", methods=["GET"])
 @jwt_required
-def get_familys():
-    familys = Family.query.all()
+def get_family_members():
+    identity = get_jwt_identity()
+    family_id = identity["family_id"]
+    members = Member.query.filter_by(family_id=family_id).all()
+    members_data = []
+    for member in members:
+        members_data.append({
+            "id": member.id,
+            "display_name": member.name,
+            "role": member.role,
+            "has_password": member.has_password
+        })
+    
+    return jsonify(members_data)
